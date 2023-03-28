e2f01f6fb8ef63e362397e12809cfb7a44e704d3
lucasrluz <lucasr.luzbr@gmail.com>
2023-03-28 15:05:32 -0300

    feat: criado modelo User para mapeamento no banco de dados

---
+++ src/main/java/com/api/blog/model/UserModel.java
@@ -0,0 +1,27 @@
+package com.api.blog.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
