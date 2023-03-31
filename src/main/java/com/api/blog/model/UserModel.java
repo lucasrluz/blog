062ce2d05c92e382064de9b9210e8924a008d07f
lucasrluz <lucasr.luzbr@gmail.com>
2023-03-31 15:21:13 -0300

    feat: adicionado métdos getters, setters e construtor no modelo User

--- src/main/java/com/api/blog/model/UserModel.java
+++ src/main/java/com/api/blog/model/UserModel.java
@@ -24,4 +24,44 @@ public class UserModel {
 
     @Column(nullable = false)
     private String password;
+
+    public UserModel(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
 }
