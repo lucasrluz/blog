73ed91a530410b4422dc0eefb175025fee92cbe8
lucasrluz <lucasr.luzbr@gmail.com>
2023-04-03 12:34:21 -0300

    feat: adicionado UserDTO para transferência de dados

---
+++ src/main/java/com/api/blog/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.api.blog.dto;
+
+public class UserDTO {
+    public String name;
+    public String email;
+    public String password;
+
+    public UserDTO(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
