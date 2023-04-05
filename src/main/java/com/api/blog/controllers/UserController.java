9e95341aef7cd5749e8552acccfae5e1decf443f
lucasrluz <lucasr.luzbr@gmail.com>
2023-04-05 13:24:58 -0300

    refactor: alteração na entrada e saída de dados para salvar usuário na classe de rota

--- src/main/java/com/api/blog/controllers/UserController.java
+++ src/main/java/com/api/blog/controllers/UserController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.blog.dto.UserDTO;
-import com.api.blog.model.UserModel;
+import com.api.blog.dto.UserDTORequest;
+import com.api.blog.dto.UserDTOResponse;
 import com.api.blog.services.UserService;
 
 @RestController
@@ -21,11 +21,11 @@ public class UserController {
     }
     
     @PostMapping
-    public ResponseEntity<Object> save(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<Object> save(@RequestBody UserDTORequest userDTORequest) {
         try {
-            UserModel userModel = this.userService.save(userDTO);
+            UserDTOResponse userDTOResponse = this.userService.save(userDTORequest);
 
-            return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(userDTOResponse);
         } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
         }
