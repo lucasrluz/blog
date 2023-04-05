8e0ac38f295cfbe5746a4bcf0f2f04be25290418
lucasrluz <lucasr.luzbr@gmail.com>
2023-04-05 13:24:08 -0300

    refactor: alteração na entrada e saída de dados para salvar usuário na classe de serviço

--- src/main/java/com/api/blog/services/UserService.java
+++ src/main/java/com/api/blog/services/UserService.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.api.blog.domain.UserDomain;
 import com.api.blog.domain.exceptions.InvalidDomainDataException;
-import com.api.blog.dto.UserDTO;
+import com.api.blog.dto.UserDTORequest;
+import com.api.blog.dto.UserDTOResponse;
 import com.api.blog.model.UserModel;
 import com.api.blog.repositories.UserRepository;
 import com.api.blog.services.util.BadRequestException;
@@ -17,15 +18,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public UserModel save(UserDTO userDTO) throws InvalidDomainDataException, BadRequestException {
-        UserDomain.validate(userDTO.name, userDTO.email, userDTO.password);
+    public UserDTOResponse save(UserDTORequest userDTORequest) throws InvalidDomainDataException, BadRequestException {
+        UserDomain.validate(userDTORequest.name, userDTORequest.email, userDTORequest.password);
 
-        if (this.userRepository.existsByEmail(userDTO.email)) {
+        if (this.userRepository.existsByEmail(userDTORequest.email)) {
             throw new BadRequestException("Error: Este e-mail já está cadastrado");
         }
 
-        UserModel userModel = new UserModel(userDTO.name, userDTO.email, userDTO.password);
+        UserModel userModel = new UserModel(userDTORequest.name, userDTORequest.email, userDTORequest.password);
 
-        return this.userRepository.save(userModel); 
+        UserModel saveUserModelResponse = this.userRepository.save(userModel);
+        
+        return new UserDTOResponse(
+            saveUserModelResponse.getUserId(),
+            saveUserModelResponse.getName(),
+            saveUserModelResponse.getEmail(),
+            saveUserModelResponse.getPassword());
     }
 }
