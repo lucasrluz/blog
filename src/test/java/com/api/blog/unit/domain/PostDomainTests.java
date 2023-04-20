7b0482a7e03a10accf0a8e18a1eb106a47234163
lucasrluz <lucasr.luzbr@gmail.com>
2023-04-20 10:55:15 -0300

    test: remoção do userId de acordo com PostDomain

--- src/test/java/com/api/blog/unit/domain/PostDomainTests.java
+++ src/test/java/com/api/blog/unit/domain/PostDomainTests.java
@@ -9,31 +9,23 @@ import com.api.blog.domain.exceptions.InvalidDomainDataException;
 public class PostDomainTests {
     @Test
     public void esperoQueNaoRetorneUmaExcecaoDadoValoresValidos() throws InvalidDomainDataException {
-        PostDomain postDomain = PostDomain.validate("foo", "foo", "foo");
+        PostDomain postDomain = PostDomain.validate("foo", "foo");
 
         Assertions.assertThat(postDomain.getTitle()).isEqualTo("foo");
         Assertions.assertThat(postDomain.getContent()).isEqualTo("foo");
-        Assertions.assertThat(postDomain.getUserId()).isEqualTo("foo");
     }
 
     @Test
     public void esperoQueRetorneUmExcecaoDadoUmTituloInvalido() {
         Assertions.assertThatExceptionOfType(InvalidDomainDataException.class)
-            .isThrownBy(() -> PostDomain.validate("", "foo", "foo"))
+            .isThrownBy(() -> PostDomain.validate("", "foo"))
             .withMessage("title: must not be blank");
     }
 
     @Test
     public void esperoQueRetorneUmExcecaoDadoUmConteudoInvalido() {
         Assertions.assertThatExceptionOfType(InvalidDomainDataException.class)
-            .isThrownBy(() -> PostDomain.validate("foo", "", "foo"))
+            .isThrownBy(() -> PostDomain.validate("foo", ""))
             .withMessage("content: must not be blank");
-    }
-
-    @Test
-    public void esperoQueRetorneUmExcecaoDadoUmUserIdInvalido() {
-        Assertions.assertThatExceptionOfType(InvalidDomainDataException.class)
-            .isThrownBy(() -> PostDomain.validate("foo", "foo", ""))
-            .withMessage("userId: must not be blank");
     }
 }
